2010a3aa5ab6d022b09bb1bd30a97e3941b54263
Hagsten <andreas.hagsten@gmail.com>
2015-12-09 21:14:06 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,6 +13,8 @@ echo "Installing DNX and its dependencies..."
 
 sudo apt-get install libunwind8 gettext libssl-dev libcurl4-openssl-dev zlib1g libicu-dev uuid-dev
 
+read
+
 dnvm upgrade -r coreclr
 
 echo "DNX installation complete"
